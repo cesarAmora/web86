59f333a56efd84cd03598f575342bf850be5278b
unknown <cesarmora1610@gmail.com>
2022-11-18 01:11:59 -0500

    proyecto 86 web

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './pedido-persona.controller';
+export * from './persona-pedido.controller';
+export * from './pedido-producto.controller';
+export * from './persona.controller';
+export * from './pedido.controller';
+export * from './producto.controller';
